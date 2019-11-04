75834e94710b70c77e6c969cef94ed37b46ffb10
Sansossio <juliosansossio@gmail.com>
2019-11-04 22:39:50 +0100

    rename functions

--- src/basic-stats/basic-stats.utils.ts
+++ src/basic-stats/basic-stats.utils.ts
@@ -1,6 +1,8 @@
 import { TftMatchEnum } from '../enums/tft-match.enum'
 import { TftMatchParticipantsModel } from '../tft-match/models/participants/tft-match.participants.interface'
 import { ITFTMatchModel } from '../tft-match/models/match/tft-match.interface'
+import * as _ from 'lodash'
+import { IQueueModel } from '../static-data/models/queue/queue.interface'
 
 // Private
 function isWin (placement?: number) {
@@ -11,6 +13,144 @@ function isWin (placement?: number) {
 }
 
 // Public
+export function winRatePerQueue (puuid: string, matches: ITFTMatchModel[]) {
+  const queues = matches.reduce<{ queueId: number, queue: Partial<IQueueModel>}[]>((prev, curr) => {
+    const {
+      queue
+    } = curr
+    const { queueId = 0 } = queue
+    if (!prev.find(p => p.queueId === queueId)) {
+      prev.push({
+        queueId,
+        queue
+      })
+    }
+    return prev
+  }, [])
+  const perQueue = queues.map((queue) => {
+    const filterMatches = matches.filter(m => m.queue.queueId === queue.queueId)
+    const winrate = calculateWinRate(puuid, filterMatches)
+    return {
+      queue: queue.queue.description,
+      queueId: queue.queueId,
+      games: filterMatches.length,
+      winrate
+    }
+  })
+  return perQueue
+}
+
+export function mostTraitsUsed (puuid: string, matches: ITFTMatchModel[]) {
+  let response: {
+    name: string,
+    num_units: number,
+    games: number
+  }[] = []
+  for (const match of matches) {
+    // Find traits
+    const { traits } = findSummoner(puuid, match.participants)
+    if (!traits) {
+      throw new Error('Invalid model (participant traits)')
+    }
+    // Iterate over traits
+    for (const trait of traits) {
+      const { name = '', num_units = 0 } = trait
+      const findIndex = response.findIndex(r => r.name === name)
+      // Upsert
+      if (findIndex !== -1) {
+        response[findIndex].num_units += num_units
+      } else {
+        response.push({
+          name,
+          num_units,
+          games: 0
+        })
+      }
+    }
+    response = response.map((val) => {
+      val.games++
+      return val
+    })
+  }
+
+  return _.orderBy(response, v => -v.games)
+    .slice(0, TftMatchEnum.MOST_TRAITS_TOTAL)
+}
+
+export function mostUnits (puuid: string, matches: ITFTMatchModel[]) {
+  const response: {
+    name?: string,
+    character_id?: string,
+    games: number,
+    tier?: number
+  }[] = []
+  for (const match of matches) {
+    const { units } = findSummoner(puuid, match.participants)
+    if (!units) {
+      throw new Error('Invalid model (participant units)')
+    }
+    for (const unit of units) {
+      const findIndex = response.findIndex(r => r.name === unit.name)
+      if (findIndex !== -1) {
+        response[findIndex].games++
+      } else {
+        response.push({
+          name: unit.name,
+          character_id: unit.character_id,
+          games: 1,
+          tier: unit.tier
+        })
+      }
+    }
+  }
+
+  return _.sortBy(response, r => -r.games)
+    .slice(0, TftMatchEnum.MOST_UNITS_TOTAL)
+}
+
+export function playersElimited (puuid: string, matches: ITFTMatchModel[]) {
+  let playersEliminated = 0
+  for (const match of matches) {
+    const { players_eliminated } = findSummoner(puuid, match.participants)
+    playersEliminated += players_eliminated || 0
+  }
+  return playersEliminated
+}
+
+export function goldLeftAverage (puuid: string, matches: ITFTMatchModel[]) {
+  let totalGold = 0
+  for (const match of matches) {
+    const { gold_left } = findSummoner(puuid, match.participants)
+    totalGold += gold_left || 0
+  }
+  return totalGold / matches.length
+}
+
+export function levelAverage (puuid: string, matches: ITFTMatchModel[]) {
+  let totalLevel = 0
+  for (const match of matches) {
+    const { level } = findSummoner(puuid, match.participants)
+    totalLevel += level || 0
+  }
+  return totalLevel / matches.length
+}
+
+export function lastRoundAverage (puuid: string, matches: ITFTMatchModel[]) {
+  let totalLastRound = 0
+  for (const match of matches) {
+    const { last_round  } = findSummoner(puuid, match.participants)
+    totalLastRound += last_round || 0
+  }
+  return totalLastRound / matches.length
+}
+
+export function winrate (puuid: string, matches: ITFTMatchModel[]) {
+  return {
+    games: matches.length,
+    winrate: calculateWinRate(puuid, matches)
+  }
+}
+
 export function findSummoner (puuid: string, participants: Partial<TftMatchParticipantsModel>[]) {
   const participant = participants.find(p => !!p.summoner && p.summoner.puuid === puuid)
   if (!participant) {
--- src/basic-stats/basic-tft-stats.service.ts
+++ src/basic-stats/basic-tft-stats.service.ts
@@ -4,10 +4,7 @@ import { ModelsName } from '../enums/database.enum'
 import { ISummonerModel } from '../summoner/models/summoner.interface'
 import { Model } from 'mongoose'
 import { ITFTMatchModel } from '../tft-match/models/match/tft-match.interface'
-import * as _ from 'lodash'
-import { calculateWinRate, findSummoner } from './basic-stats.utils'
-import { IQueueModel } from '../static-data/models/queue/queue.interface'
-import { TftMatchEnum } from '../enums/tft-match.enum'
+import * as utils from './basic-stats.utils'
 
 @Injectable()
 export class BasicTftStatsService {
@@ -17,144 +14,6 @@ export class BasicTftStatsService {
 
     @InjectModel(ModelsName.TFT_MATCH) private readonly tftRepository: Model<ITFTMatchModel>
   ) {}
-
-  private winRatePerQueue (puuid: string, matches: ITFTMatchModel[]) {
-    const queues = matches.reduce<{ queueId: number, queue: Partial<IQueueModel>}[]>((prev, curr) => {
-      const {
-        queue
-      } = curr
-      const { queueId = 0 } = queue
-      if (!prev.find(p => p.queueId === queueId)) {
-        prev.push({
-          queueId,
-          queue
-        })
-      }
-      return prev
-    }, [])
-    const perQueue = queues.map((queue) => {
-      const filterMatches = matches.filter(m => m.queue.queueId === queue.queueId)
-      const winrate = calculateWinRate(puuid, filterMatches)
-      return {
-        queue: queue.queue.description,
-        queueId: queue.queueId,
-        games: filterMatches.length,
-        winrate
-      }
-    })
-    return perQueue
-  }
-
-  private mostTraitsUsed (puuid: string, matches: ITFTMatchModel[]) {
-    let response: {
-      name: string,
-      num_units: number,
-      games: number
-    }[] = []
-    for (const match of matches) {
-      // Find traits
-      const { traits } = findSummoner(puuid, match.participants)
-      if (!traits) {
-        throw new Error('Invalid model (participant traits)')
-      }
-      // Iterate over traits
-      for (const trait of traits) {
-        const { name = '', num_units = 0 } = trait
-        const findIndex = response.findIndex(r => r.name === name)
-        // Upsert
-        if (findIndex !== -1) {
-          response[findIndex].num_units += num_units
-        } else {
-          response.push({
-            name,
-            num_units,
-            games: 0
-          })
-        }
-      }
-      response = response.map((val) => {
-        val.games++
-        return val
-      })
-    }
-
-    return _.orderBy(response, v => -v.games)
-      .slice(0, TftMatchEnum.MOST_TRAITS_TOTAL)
-  }
-
-  private mostUnits (puuid: string, matches: ITFTMatchModel[]) {
-    const response: {
-      name?: string,
-      character_id?: string,
-      games: number,
-      tier?: number
-    }[] = []
-    for (const match of matches) {
-      const { units } = findSummoner(puuid, match.participants)
-      if (!units) {
-        throw new Error('Invalid model (participant units)')
-      }
-      for (const unit of units) {
-        const findIndex = response.findIndex(r => r.name === unit.name)
-        if (findIndex !== -1) {
-          response[findIndex].games++
-        } else {
-          response.push({
-            name: unit.name,
-            character_id: unit.character_id,
-            games: 1,
-            tier: unit.tier
-          })
-        }
-      }
-    }
-
-    return _.sortBy(response, r => -r.games)
-      .slice(0, TftMatchEnum.MOST_UNITS_TOTAL)
-  }
-
-  private playersElimited (puuid: string, matches: ITFTMatchModel[]) {
-    let playersEliminated = 0
-    for (const match of matches) {
-      const { players_eliminated } = findSummoner(puuid, match.participants)
-      playersEliminated += players_eliminated || 0
-    }
-    return playersEliminated
-  }
-
-  private averageGoldLeft (puuid: string, matches: ITFTMatchModel[]) {
-    let totalGold = 0
-    for (const match of matches) {
-      const { gold_left } = findSummoner(puuid, match.participants)
-      totalGold += gold_left || 0
-    }
-    return totalGold / matches.length
-  }
-
-  private averageLevel (puuid: string, matches: ITFTMatchModel[]) {
-    let totalLevel = 0
-    for (const match of matches) {
-      const { level } = findSummoner(puuid, match.participants)
-      totalLevel += level || 0
-    }
-    return totalLevel / matches.length
-  }
-
-  private averageLastRound (puuid: string, matches: ITFTMatchModel[]) {
-    let totalLastRound = 0
-    for (const match of matches) {
-      const { last_round  } = findSummoner(puuid, match.participants)
-      totalLastRound += last_round || 0
-    }
-    return totalLastRound / matches.length
-  }
-
-  private globalWinRate (puuid: string, matches: ITFTMatchModel[]) {
-    return {
-      games: matches.length,
-      winrate: calculateWinRate(puuid, matches)
-    }
-  }
 
   async updateSummoner ({ _id, puuid }: ISummonerModel) {
     const matchHistory = await this.tftRepository.find({
@@ -166,20 +25,20 @@ export class BasicTftStatsService {
 
     return {
       globalAverage: {
-        goldLeft: this.averageGoldLeft(puuid, matchHistory),
-        level: this.averageLevel(puuid, matchHistory),
-        lastRound: this.averageLastRound(puuid, matchHistory)
+        goldLeft: utils.goldLeftAverage(puuid, matchHistory),
+        level: utils.levelAverage(puuid, matchHistory),
+        lastRound: utils.lastRoundAverage(puuid, matchHistory)
       },
       global: {
-        playersEliminated: this.playersElimited(puuid, matchHistory),
-        winrate: this.globalWinRate(puuid, matchHistory)
+        playersEliminated: utils.playersElimited(puuid, matchHistory),
+        winrate: utils.winrate(puuid, matchHistory)
       },
       perQueue: {
-        winrate: this.winRatePerQueue(puuid, matchHistory)
+        winrate: utils.winRatePerQueue(puuid, matchHistory)
       },
       mostUsed: {
-        traits: this.mostUnits(puuid, matchHistory),
-        units: this.mostTraitsUsed(puuid, matchHistory)
+        traits: utils.mostUnits(puuid, matchHistory),
+        units: utils.mostTraitsUsed(puuid, matchHistory)
       }
     }
   }
