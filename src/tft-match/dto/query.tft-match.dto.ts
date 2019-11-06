f70f8ca38905c55e4b6fef28a2fb97a4bee7bc90
Julio Sansossio <J.Sansossio@payvision.com>
2019-11-06 11:49:02 +0100

    Number type function

--- src/tft-match/dto/query.tft-match.dto.ts
+++ src/tft-match/dto/query.tft-match.dto.ts
@@ -3,18 +3,19 @@ import { ApiModelProperty } from '@nestjs/swagger'
 import { IsInt, Max, Min, IsNotEmpty } from 'class-validator'
 import { Type } from 'class-transformer'
 import { LimitsEnum } from '../../enums/limits.enum'
+import { typeNumber } from '../../base/base.utils'
 
 export class QueryTftMatches extends GetSummonerQueryDTO {
   @ApiModelProperty()
   @IsNotEmpty()
-  @Type(() => Number)
+  @Type(typeNumber)
   @IsInt()
   @Max(LimitsEnum.MATCH_LIMIT_MAX)
   @Min(LimitsEnum.MATCH_LIMIT_MIN)
   limit!: number
 
   @ApiModelProperty()
-  @Type(() => Number)
+  @Type(typeNumber)
   @IsInt()
   @Min(LimitsEnum.MATCH_PAGE_MIN)
   @IsNotEmpty()
